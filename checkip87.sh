78f2939690eaf6fe05c7dd10e09ccb98d3dbbecb
Nguyen Thanh Bien <bien@bienlab.com>
2024-08-07 14:08:14 +0700

    Update checkip87.sh

--- checkip87.sh
+++ checkip87.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
  
-ip a | grep '10.209.52.87'
+ip a | grep '10.10.10.87'
  
 if [ $? -eq 0 ]
 then
   echo "this is active node"
-  /home/vt_admin/getmetrics/start_dnsmon.sh
-  /home/vt_admin/getmetrics/start_getmetrics.sh
+  /home/admin/getmetrics/start_dnsmon.sh
+  /home/admin/getmetrics/start_getmetrics.sh
 else
   echo "this is standby node"
-  /home/vt_admin/getmetrics/stop_dnsmon.sh
-  /home/vt_admin/getmetrics/stop_getmetrics.sh
+  /home/admin/getmetrics/stop_dnsmon.sh
+  /home/admin/getmetrics/stop_getmetrics.sh
 fi
 docker ps -a
 
