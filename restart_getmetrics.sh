476d87e1e18ed35a097874b341a6740435326f15
Nguyen Thanh Bien <bien@bienlab.com>
2024-08-07 14:07:18 +0700

    Update restart_getmetrics.sh

--- restart_getmetrics.sh
+++ restart_getmetrics.sh
@@ -2,10 +2,10 @@
 echo "starting getmetrics-cache"
 docker stop getmetrics-cache
 docker rm getmetrics-cache
-docker run -d --name getmetrics-cache -v /home/vt_admin/getmetrics/caches/devicelist.json:/usr/src/app/devicelist.json -v /home/vt_admin/getmetrics/connection.py:/usr/src/app/connection.py --restart unless-stopped getmetrics
+docker run -d --name getmetrics-cache -v /home/admin/getmetrics/caches/devicelist.json:/usr/src/app/devicelist.json -v /home/admin/getmetrics/connection.py:/usr/src/app/connection.py --restart unless-stopped getmetrics
 
 echo "starting getmetrics-resolver"
 docker stop getmetrics-resolver
 docker rm getmetrics-resolver
-docker run -d --name getmetrics-resolver -v /home/vt_admin/getmetrics/resolvers/devicelist.json:/usr/src/app/devicelist.json -v /home/vt_admin/getmetrics/connection.py:/usr/src/app/connection.py --restart unless-stopped getmetrics
+docker run -d --name getmetrics-resolver -v /home/admin/getmetrics/resolvers/devicelist.json:/usr/src/app/devicelist.json -v /home/admin/getmetrics/connection.py:/usr/src/app/connection.py --restart unless-stopped getmetrics
 
